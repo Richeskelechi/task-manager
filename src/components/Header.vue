2a7e243d86fd13c49e519ba54413695dd2413c3e
mikezalesky43@gmail.com <richeskelechi94.rk@gmail.com>
2022-02-01 15:28:40 +0100

    Done With The Task Manager

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  components: {
+    Button,
+  },
+  props: {
+    title: {
+      type: String,
+      default: "Hello Riches", //the default will show if the prop has no value.
+    },
+    showAddTask: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
